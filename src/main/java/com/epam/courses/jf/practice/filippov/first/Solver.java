b47b90446a646710804a23c51307facf313d46e1
elefus <elefus@yandex.ru>
2017-08-11 13:29:00 +0300

    Added stub for the first task.

---
+++ src/main/java/com/epam/courses/jf/practice/filippov/first/Solver.java
@@ -0,0 +1,11 @@
+package com.epam.courses.jf.practice.filippov.first;
+
+import com.epam.courses.jf.practice.common.first.ISolver;
+
+/**
+ * Реализация первого блока заданий.
+ */
+public class Solver implements ISolver {
+
+    // TODO выполнение задания
+}
