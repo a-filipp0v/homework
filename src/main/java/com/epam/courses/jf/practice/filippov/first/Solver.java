9ea73bf16e75941d224c35e638b25c8569f5f2bb
filippov <tkhnote@yandex.ru>
2017-08-16 18:20:11 +0300

    1-5, 18

--- src/main/java/com/epam/courses/jf/practice/filippov/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/filippov/first/Solver.java
@@ -2,10 +2,221 @@ package com.epam.courses.jf.practice.filippov.first;
 
 import com.epam.courses.jf.practice.common.first.ISolver;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Реализация первого блока заданий.
  */
 public class Solver implements ISolver {
 
     // TODO выполнение задания
+
+    @Override
+    public void task1() {
+        Scanner sc = new Scanner(System.in);
+        String[] lines = new String[Integer.parseInt(sc.nextLine())];
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = sc.nextLine();
+        }
+        sc.close();
+        int minLength = 0;
+        int maxLength = 0;
+        String minString = null;
+        String maxString = null;
+        for (String s : lines) {
+            if (s.length() >= maxLength) {
+                maxLength = s.length();
+                maxString = s;
+            }
+            if (s.length() <= minLength) {
+                minLength = s.length();
+                minString = s;
+            }
+        }
+        System.out.printf("MIN (%d): \"%s\"%n", minLength, minString);
+        System.out.printf("MAX (%d): \"%s\"%n", maxLength, maxString);
+    }
+
+    @Override
+    public void task2() {
+        Scanner sc = new Scanner(System.in);
+        String[] lines = new String[Integer.parseInt(sc.nextLine())];
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = sc.nextLine();
+        }
+        for (int i = 0; i < lines.length; i++) {
+            for (int j = 0; j < lines.length - 1; j++) {
+                if (lines[j].length() > lines[j + 1].length()) {
+                    String temp = lines[j];
+                    lines[j] = lines[j + 1];
+                    lines[j + 1] = temp;
+                } else if (lines[j].length() == lines[j + 1].length()) {
+                    if (lines[j].compareTo(lines[j + 1]) >= 0) {
+                        String temp = lines[j];
+                        lines[j] = lines[j + 1];
+                        lines[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        for (String st : lines) {
+            System.out.printf("(%d): \"%s\"%n", st.length(), st);
+        }
+    }
+
+    @Override
+    public void task3() {
+        Scanner sc = new Scanner(System.in);
+        String[] lines = new String[Integer.parseInt(sc.nextLine())];
+        int sumOfLenghts = 0;
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = sc.nextLine();
+            sumOfLenghts += lines[i].length();
+        }
+        int averageLength = (int) Math.floor(sumOfLenghts / lines.length);
+        System.out.printf("AVERAGE (%d)%n", averageLength);
+        for (String s : lines) {
+            if (s.length() < averageLength) {
+                System.out.printf("(%d): \"%s\"%n", s.length(), s);
+            }
+        }
+
+    }
+
+    @Override
+    public void task4() {
+        Scanner sc = new Scanner(System.in);
+        String[] lines = sc.nextLine().split(" ");
+        String foundWord = lines[0];
+        long amountOfUniqueSymbols = lines[0].chars().distinct().count();
+        for (String s : lines) {
+            if (s.chars().distinct().count() <= amountOfUniqueSymbols) {
+                amountOfUniqueSymbols = s.chars().distinct().count();
+                foundWord = s;
+            }
+        }
+        System.out.println(foundWord);
+        System.out.println(amountOfUniqueSymbols);
+    }
+
+    @Override
+    public void task5() {
+        Scanner sc = new Scanner(System.in);
+        String[] inputLines = sc.nextLine().split(" ");
+        String vowels = "aeiouyAEIOUY";
+        int numOfFoundWords = 0;
+        for (String s : inputLines) {
+            int numOfVowels = 0;
+            int numOfConsonants = 0;
+            if (s.matches("[a-zA-Z]+")) {
+                for (int i = 0; i < s.length(); i++) {
+                    if (vowels.indexOf(s.charAt(i)) >= 0) {
+                        numOfVowels++;
+                    } else if (Character.isLetter(s.charAt(i))) {
+                        numOfConsonants++;
+                    }
+                }
+                if (numOfConsonants == numOfVowels) {
+                    numOfFoundWords++;
+                }
+            }
+        }
+        System.out.println(numOfFoundWords);
+    }
+
+    @Override
+    public void task6() {
+
+    }
+
+    @Override
+    public void task18() {
+
+//        Scanner scanner = new Scanner(System.in);
+//        final int matrixSize = Integer.parseInt(scanner.next());
+//        String[][] matrix = new String[matrixSize][matrixSize];
+//        for (int row = 0; row < matrixSize; ++row) {
+//            for (int col = 0; col < matrixSize; ++col) {
+//                matrix[row][col] = scanner.next();
+//            }
+//        }
+//
+//        int max = Integer.parseInt(matrix[0][0]);
+//        for (int i = matrixSize - 1; i >= 0; i--) {
+//            for (int j = matrixSize - 1; j >= 0; j--) {
+//                if (String.valueOf(max).equals(matrix[i][j])) {
+//                    for (int k = matrixSize - 1; k >= 0; k--) {
+//                        matrix[i][k] = "";
+//                        matrix[k][j] = "";
+//                    }
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < matrixSize; i++) {
+//            for (int j = 0; j < matrixSize; j++) {
+//                if (!matrix[i][j].isEmpty())
+//                    System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        Scanner sc = new Scanner(System.in);
+        int matrixSize = Integer.parseInt(sc.next());
+        List<List<Integer>> matrix = new ArrayList<>();
+        Integer maxVal = null;
+        for (int i = 0; i < matrixSize; i++) {
+            matrix.add(new ArrayList<>());
+            for (int j = 0; j < matrixSize; j++) {
+                int temp = Integer.parseInt(sc.next());
+                matrix.get(i).add(temp);
+                if (maxVal==null || maxVal < temp)
+                    maxVal = temp;
+            }
+        }
+
+        ArrayList<Integer> deleteColumns = new ArrayList<>();
+        ArrayList<Integer> deleteRows = new ArrayList<>();
+
+        for (List<Integer> list : matrix) {
+            if (list.contains(maxVal))
+            {
+                if (!deleteRows.contains(maxVal))
+                    deleteRows.add(matrix.indexOf(list));
+            }
+            for (int i = 0; i < list.size(); i++) {
+                if (maxVal.equals(list.get(i))) {
+                    if (!deleteColumns.contains(i))
+                    deleteColumns.add(i);
+                }
+            }
+        }
+
+        deleteColumns.sort(Comparator.reverseOrder());
+        deleteRows.sort(Comparator.reverseOrder());
+
+        for (int index : deleteRows) {
+            matrix.remove(index);
+        }
+        for (List<Integer> list : matrix) {
+            for (int index : deleteColumns) {
+                list.remove(index);
+            }
+        }
+
+        for (List<Integer> list : matrix) {
+            for(Integer i : list) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solver sv = new Solver();
+        sv.task18();
+    }
 }
