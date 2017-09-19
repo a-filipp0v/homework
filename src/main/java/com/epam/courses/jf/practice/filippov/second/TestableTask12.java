32a82d66366ba5a445bbfd7d6c7fc62dc90e182d
filippov <tkhnote@yandex.ru>
2017-09-19 15:59:53 +0300

    2/->12

--- src/main/java/com/epam/courses/jf/practice/filippov/second/TestableTask12.java
+++ src/main/java/com/epam/courses/jf/practice/filippov/second/TestableTask12.java
@@ -10,22 +10,7 @@ public class TestableTask12 implements ITestableTask12 {
     @Override
     public List<Integer> transform(List<Integer> integers, int value) {
         integers.sort(Comparator.naturalOrder());
-        for (Integer integer : integers) {
-            if (value < integer) {
-                integers.add(integers.indexOf(integer), value);
-                break;
-            }
-        }
         return integers;
     }
 }
-//class Test{
-//    public static void main(String[] args) {
-//        TestableTask12 ts = new TestableTask12();
-//        List<Integer> ls = new ArrayList<>(Arrays.asList(1, 9, 2, 4, 2, 0, 8, 11, 12, 8, 7));
-//        ts.transform(ls, 5);
-//        for (Integer l : ls) {
-//            System.out.print(l + " ");
-//        }
-//    }
-//}
+
