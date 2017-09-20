8ff8a347a140eea086549f0a0f8d2a8cc76cd001
filippov <tkhnote@yandex.ru>
2017-09-20 16:35:27 +0300

    2/->15

---
+++ src/main/java/com/epam/courses/jf/practice/filippov/second/TestableTask15.java
@@ -0,0 +1,15 @@
+/*19.09.2017*/
+package com.epam.courses.jf.practice.filippov.second;
+
+import com.epam.courses.jf.practice.common.second.I2DPoint;
+import com.epam.courses.jf.practice.common.second.ITestableTask15;
+
+import java.io.File;
+import java.util.Set;
+
+public class TestableTask15 implements ITestableTask15 {
+    @Override
+    public IFileWithLines analyze(Set<I2DPoint> points, File output) {
+        return null;
+    }
+}
